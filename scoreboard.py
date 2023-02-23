04c644900bbe45f1b8313b95193178938dda2a1e
kingdreamerr <jackmvula458@gmail.com>
2023-02-23 14:07:44 +0200

    Add methods to the scoreboard to update the score and and say when the game is over

--- scoreboard.py
+++ scoreboard.py
@@ -12,3 +12,15 @@ class Scoreboard(Turtle):
         self.score = 0
         self.goto(-255, 250)
         self.update_screen()
+
+    def update_screen(self):
+        self.clear()
+        self.write(f"Score:{self.score}", align='left', font=FONT)
+
+    def add_score(self):
+        self.score += 1
+        self.update_screen()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER!", align='center', font=FONT)
