23c4da44f27ca695453c12fb13db9d64f6ec29f1
kingdreamerr <jackmvula458@gmail.com>
2023-02-23 14:11:11 +0200

    Add condition to check if the player has reached the top of the screen

--- main.py
+++ main.py
@@ -20,17 +20,14 @@ while game_is_on:
 
     car_manager.create_car()
     car_manager.move()
-    screen.update()import time
-from turtle import Screen
-from player import Player
-from car_manager import CarManager
-from scoreboard import Scoreboard
+    screen.update()
 
-screen = Screen()
-scoreboard = Scoreboard()
-screen.setup(width=600, height=600)
-screen.tracer(0)
-screen.listen()
+#     detect collision with car
+
+    if player.finished():
+        player.start()
+        scoreboard.add_score()
+        car_manager.level_up()
 
 
 
