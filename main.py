6b4d899ea2b30ccca8dd852372076583467dd4a4
kingdreamerr <jackmvula458@gmail.com>
2023-02-23 14:09:20 +0200

    Add instances of the socoreboard, carManager, and player classes

--- main.py
+++ main.py
@@ -9,6 +9,28 @@ scoreboard = Scoreboard()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 screen.listen()
+player = Player()
+
+screen.onkeypress(player.move, 'Up')
+game_is_on = True
+car_manager = CarManager()
+
+while game_is_on:
+    time.sleep(0.1)
+
+    car_manager.create_car()
+    car_manager.move()
+    screen.update()import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+scoreboard = Scoreboard()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+screen.listen()
 
 
 
